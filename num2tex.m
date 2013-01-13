000c51fb4fbbae274c4f13f2795aec54990962f3
Roald Frederickx <roald.frederickx@gmail.com>
2013-01-13 19:55:12 +0100

    num2tex & num2texh

---
+++ num2tex.m
@@ -0,0 +1,26 @@
+% This does a num2str, but with latex format and instead of 1.23e42 we 
+% return 1e23 \cdot 10^{42}
+%
+% Exponent notation is used for numbers outside the range 1e-4 to 1e4 (non inclusive).
+% 
+% function num2tex(num, sd)
+% sd = optional: the number of significant digits requested
+function str = num2tex(num, sd)
+
+if num == 0
+	str = '0';
+	return
+end
+
+if nargin >= 2
+	num = roundsd(num, sd);
+end
+
+exponent = floor(log10(abs(num)));
+mantissa = num / 10^exponent;
+
+if -3 <= exponent && exponent <= 3
+	str = num2str(num);
+else
+	str = [num2str(mantissa),' \cdot 10^{',num2str(exponent),'}'];
+end
---
+++ num2texh.m
@@ -0,0 +1,35 @@
+% This does a num2str, but with latex format and 'human-readable' power 
+% indicators, e.g. instead of 1.23e10, we return 12.3\,G 
+%
+% mili is only used between 0.009999.. and 0.001
+%
+% function num2texh(num, sd)
+% sd = optional: the number of significant digits requested
+function str = num2texh(num, sd)
+
+if num == 0
+	str = '0';
+	return
+end
+
+if nargin >= 2
+	num = roundsd(num, sd);
+end
+
+prefixes = {'\,y','\,z','\,a','\,f','\,p','\,n','\,\mu','\,m','','\,k','\,M','\,G','\,T','\,P','\,E','\,Z','\,Y'};
+
+exponent = floor(log10(abs(num)));
+powerBy3 = floor(exponent/3);
+prefix = prefixes{powerBy3 + 9};
+mantissa = num / 10^(3*powerBy3);
+
+
+exponent = floor(log10(abs(num)));
+mantissa = num / 10^exponent;
+
+%This can be commented to allow for mili to be used form 0.999 downwards
+if -2 <= exponent && exponent <= 0
+	str = num2str(num);
+else
+	str = [num2str(mantissa), prefix];
+end
