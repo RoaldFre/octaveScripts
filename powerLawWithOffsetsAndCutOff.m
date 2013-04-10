4f8079741dd0ffe9fab53017eddfa8b22b061f07
Roald Frederickx <roald.frederickx@gmail.com>
2013-04-10 12:46:43 +0200

    power law: no cut off for small x

--- powerLawWithOffsetsAndCutOff.m
+++ powerLawWithOffsetsAndCutOff.m
@@ -3,5 +3,14 @@ function ys = powerLawWithOffsetsAndCutOff(xs, exponent, x1, xOffset, yOffset, c
 % do the cut off as a transformation on the x variables TODO best way? Directly work on ys?
 xTransformed = cutOff + (-cutOffWidth * log(1 + exp((cutOff - xs)/cutOffWidth)));
 
+% for small values of x, the correction is negligible and there should be 
+% no effect of this cut off. However, if x -> 0, even the tinyest 
+% contribution from the cut off will show significantly on a loglog scale. 
+% Hence, sufficiently far away from the cut off, we use the exact input 
+% values of xs.
+% TODO: smoother transition?
+untouchedMask = xTransformed < cutOff - 4*cutOffWidth;
+xTransformed(untouchedMask) = xs(untouchedMask);
+
 ys = yOffset + ((xTransformed + xOffset) / x1).^exponent;
 
--- powerLawWithOffsetsAndCutOffLogLog.m
+++ powerLawWithOffsetsAndCutOffLogLog.m
@@ -4,6 +4,16 @@ function ys = powerLawWithOffsetsAndCutOffLogLog(xs, exponent, x1, xOffset, yOff
 % do the cut off as a transformation on the x variables TODO best way? Directly work on ys?
 xTransformed = cutOff + (-cutOffWidth * log(1 + exp((cutOff - xs)/cutOffWidth)));
 
+% for small values of x, the correction is negligible and there should be 
+% no effect of this cut off. However, if x -> 0, even the tinyest 
+% contribution from the cut off will show significantly on a loglog scale. 
+% Hence, sufficiently far away from the cut off, we use the exact input 
+% values of xs.
+% TODO: smoother transition?
+untouchedMask = xTransformed < cutOff - 4*cutOffWidth;
+xTransformed(untouchedMask) = xs(untouchedMask);
+
+
 ys = yOffset + ((xTransformed + xOffset) / x1).^exponent;
 ys = real(ys); % TODO justifyable? Better way?
 
