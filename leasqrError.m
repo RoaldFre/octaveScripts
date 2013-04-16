d9602add97b23c45627b18ec59e9bb04b8f3ab26
Roald Frederickx <roald.frederickx@gmail.com>
2013-04-16 15:02:42 +0200

    bounds in leasqrError

--- leasqrError.m
+++ leasqrError.m
@@ -1,28 +1,42 @@
-% leasqrError: version of leasqr that does crude error propagation of the input
+% leasqrError: version of leasqr that does a *very crude* error propagation 
+% of the input uncertainties (assuming no correlation between data points).
 % pErr: errors on p due to input errors in y (yErr = stdDev of y)
 function [f,p,pErr,cvg,iter,corp,covp,covr,stdresid,Z,r2] = ...
-      leasqrError(x,y,yErr,pin,F,numPerturbed,stol,niter)
+      leasqrError(x,y,yErr,pin,F,numPerturbed,bounds,stol,niter)
 
 if (nargin < 6) numPerturbed = 50; end
-if (nargin < 7) stol = 1e-5; end
-if (nargin < 8) niter = 1000; end
+if (nargin < 7 || isempty(bounds)) bounds = ones(numel(pin), 1) * [-Inf Inf]; end
+if (nargin < 8) stol = 1e-5; end
+if (nargin < 9) niter = 1000; end
 
-wt = 1./yErr;
-
-[f,p,cvg,iter,corp,covp,covr,stdresid,Z,r2] = leasqr(x,y,pin,F,stol,niter,wt);
-pErrFit = sqrt(diag(covp));
+options.bounds = bounds;
 
 pin = pin(:); % make it a column vector
+x = x(:);
 y = y(:);
 yErr = yErr(:);
+
+wt = 1./yErr;
+dp = 0.001 * ones(size(pin));
+dFdp = "dfdp";
+
+[f,p,cvg,iter,corp,covp,covr,stdresid,Z,r2] = leasqr(x,y,pin,F,stol,niter,wt,dp,dFdp,options);
+pErrFit = sqrt(diag(covp));
+if (cvg != 1)
+	printf("\nCONVERGENCE NOT ACHIEVED!\n\n");
+end
+
 ps = zeros(numel(pin), numPerturbed);
 moreWasOn = page_screen_output;
 more off;
 for i = 1:numPerturbed
 	printf("\rPerturbing: sample %d of %d", i, numPerturbed);
-	yPerturbed = y + randn(numel(y), 1) .* yErr;
+	yPerturbed = y + randn(size(y)) .* yErr;
 	%plot(x,yPerturbed); sleep(1);
-	[_f,_p] = leasqr(x,yPerturbed,pin,F,stol,niter,wt);
+	[_f,_p, _cvg] = leasqr(x,yPerturbed,pin,F,stol,niter,wt,dp,dFdp,options);
+	if (_cvg != 1)
+		printf("\nCONVERGENCE NOT ACHIEVED!\n\n");
+	end
 	ps(:,i) = _p;
 end
 printf("\n");
