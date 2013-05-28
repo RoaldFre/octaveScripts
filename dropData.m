88688ebcb3e3b665fbe0a2ce650e13b2b953f6b2
Roald Frederickx <roald.frederickx@gmail.com>
2013-05-28 23:13:22 +0200

    dropData also for 2D arrays

--- dropData.m
+++ dropData.m
@@ -1,4 +1,6 @@
 % if period == 3, only return the 1st, 4rd, 7th, 10th, ... data sample.
+%
+% If the input is a 2D array, then only columns get dropped
 function ys = dropData(xs, period)
 
 if period == 1
@@ -6,9 +8,17 @@ if period == 1
 	return
 end
 
-inputSize = numel(xs);
-finalSize = floor(inputSize / period);
+if isvector(xs)
+	inputSize = numel(xs);
+	finalSize = floor(inputSize / period);
 
-indices = 1 + period*(0:(finalSize-1));
-ys = xs(indices);
+	indices = 1 + period*(0:(finalSize-1));
+	ys = xs(indices);
+else
+	numSamples = size(xs, 2);
+	finalSamples = floor(numSamples / period);
+
+	indices = 1 + period*(0:(finalSamples-1));
+	ys = xs(:, indices);
+end
 
