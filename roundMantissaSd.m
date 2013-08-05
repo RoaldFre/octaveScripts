6bdf2242d5747f1d83e50ad198b1e3ed0d2dc122
Roald Frederickx <roald.frederickx@gmail.com>
2013-08-05 22:21:29 +0200

    roundsdStr & roundMantissaSd: num2str -> sprintf

--- roundMantissaSd.m
+++ roundMantissaSd.m
@@ -5,10 +5,6 @@
 % otherwise the rounding may cause the number to get >= 10.
 function str = roundMantissaSd(num, sd)
 
-if sd > 5
-	error("Can't handle sd>5. Rewrite myself to something less hacky.")
-end
-
 if abs(num) < 1 || abs(num) >= 10
 	error("Need a number between 1 and 10!");
 end
@@ -24,16 +20,5 @@ if abs(num) >= 10
 	error("Rounding the number caused it to get abs(num) >= 10!");
 end
 
-str = num2str(num);
-digits = sum(str != '.');
-
-if digits == sd
-	return
-end
-
-if digits == 1 && sd > 1
-	str = [str, '.'];
-end
-
-str = [str, '0'*ones(1,sd - digits)];
+str = sprintf(['%.',num2str(sd-1),'f'], num)
 
--- roundsdStr.m
+++ roundsdStr.m
@@ -1,10 +1,5 @@
 function str = roundsdStr(num, sd)
 
-% I'm using num2str all over the place, and that only fives 5 significant digits
-if sd > 5
-	error("Can't handle sd>5. Rewrite myself to something less hacky.")
-end
-
 if sd <= 0
 	str = '0';
 	return
@@ -16,9 +11,13 @@ num = roundsd(num, sd);
 if abs(num) < 1
 	% Leading 0.0*
 	% Can just plop trailing 0's at the back
-	rawstr = num2str(num);
-	digits = numel(rawstr) - find(rawstr == '.');
-	str = [rawstr,'0'*ones(1, sd - digits)];
+	leadingZerosAfterPoint = floor(-log10(abs(num)));
+	if (log10(abs(num)) == round(log10(abs(num))));
+		% We are a perfect power of 10, in that case, we are off by one
+		leadingZerosAfterPoint -= 1;
+	end
+	totalDigitsAfterPoint = leadingZerosAfterPoint + sd;
+	str = sprintf(['%.',num2str(totalDigitsAfterPoint),'f'],num);
 	return
 end
 
@@ -36,21 +35,4 @@ if digitsAfterDecimalPoint < 0
 	error("More digits in number than requested significant digits. Use scientific notation!");
 end
 
-str = num2str(num);
-
-if digitsAfterDecimalPoint == 0
-	return
-end
-
-digits = sum(str != '.');
-
-if digits == sd
-	return
-end
-
-% need to append zeros
-if digits == (numExponent + 1)
-	str = [str, '.'];
-end
-
-str = [str,'0'*ones(1, sd - digits)];
+str = sprintf(['%.',num2str(digitsAfterDecimalPoint),'f'], num);
