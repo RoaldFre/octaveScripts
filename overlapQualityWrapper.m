0d6fd10a1af5d105644fce613831ac88dd4398ad
Roald Frederickx <roald.frederickx@gmail.com>
2013-08-06 02:03:32 +0200

    overlapQualityWrapper: loglog option
    measure overlap on loglog plot

--- overlapQualityWrapper.m
+++ overlapQualityWrapper.m
@@ -9,9 +9,19 @@
 %
 %  WARNING! The xOffsets are multiplied with 1e-12 to get them in the same 
 %  range as the yOffsets for the minimizer that calls this wrapper!
-function S = overlapQualityWrapper(exponentsAndOffsets, scalingFunction, Ns, xs, ys, dys)
+function S = overlapQualityWrapper(exponentsAndOffsets, scalingFunction, Ns, xs, ys, dys, loglog)
+
+if nargin < 7; loglog = false; end
 
 [xs, ys, dys] = feval(scalingFunction, exponentsAndOffsets, Ns, xs, ys, dys);
 
-S = overlapQuality(xs, ys, dys)
+if loglog
+	%S = overlapQuality(log(xs), log(ys), dys ./ ys)
+	logxs  = cellfun(@log, xs, 'UniformOutput',false);
+	logys  = cellfun(@log, ys, 'UniformOutput',false);
+	dlogys = cellfun(@(a,b) a ./ b, dys, ys, 'UniformOutput',false);
+	S = overlapQuality(logxs, logys, dlogys)
+else
+	S = overlapQuality(xs, ys, dys)
+end
 
