d271e8ebb94aea3bc578a0652c8c68ac7457fd69
Roald Frederickx <roald.frederickx@gmail.com>
2013-08-17 23:42:26 +0200

    plotColors: printer friendly jet

--- finiteSizeResultplot.m
+++ finiteSizeResultplot.m
@@ -12,16 +12,14 @@ avgPs = zeros(1, size(clustPs, 2));
 errPs = zeros(1, size(clustPs, 2));
 
 hold on;
-plotColors = colormap;
+colors = plotColors(size(clustPs,2));
 handles = zeros(1, size(clustPs,2));
 for i = 1:size(clustPs,2)
-	plotColorsI = round(size(plotColors, 1) * i / size(clustPs,2));
-
 	ys = clustPs(:,i);
 	dys = clustPErrs(:,i);
 	h = ploterror(clustNs, ys, dys);
 	handles(i) = h;
-	set(h ,'Color', plotColors(plotColorsI,:));
+	set(h ,'Color', colors(i,:));
 
 	ys  = ys (meanStartIndex:end);
 	dys = dys(meanStartIndex:end);
@@ -29,9 +27,9 @@ for i = 1:size(clustPs,2)
 	y = sum(ys .* wts) / sum(wts);
 	dy = sqrt(1/sum(wts));
 	xs = clustNs([meanStartIndex, end]);
-	h = plot(xs, y * [1 1]);      set(h ,'Color', plotColors(plotColorsI,:));
-	h = plot(xs, (y-dy) * [1 1]); set(h ,'Color', plotColors(plotColorsI,:));
-	h = plot(xs, (y+dy) * [1 1]); set(h ,'Color', plotColors(plotColorsI,:));
+	h = plot(xs, y * [1 1]);      set(h ,'Color', colors(i,:));
+	h = plot(xs, (y-dy) * [1 1]); set(h ,'Color', colors(i,:));
+	h = plot(xs, (y+dy) * [1 1]); set(h ,'Color', colors(i,:));
 
 	printf('Mean result: %f +- %f\n', y, dy);
 	avgPs(i) = y;
